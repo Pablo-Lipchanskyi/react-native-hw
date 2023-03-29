332f90b393eb0293ca6122e9c5368f3f55b611ac
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-03-29 13:16:21 +0300

    comm

---
+++ myNewProject/firebase/config.js
@@ -0,0 +1,22 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGw6_H9IKWhlb7dhU5UHVxfly1LvOT6Qo",
+  authDomain: "react-native-hw-d116c.firebaseapp.com",
+  projectId: "react-native-hw-d116c",
+  storageBucket: "react-native-hw-d116c.appspot.com",
+  messagingSenderId: "515724937374",
+  appId: "1:515724937374:web:55ac4580de60c29bc9538f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
