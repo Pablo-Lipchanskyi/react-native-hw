329f4f76ec45b62f51d1abb5f487ae05fc103ddb
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-03-30 10:17:30 +0300

    Update config.js

--- myNewProject/firebase/config.js
+++ myNewProject/firebase/config.js
@@ -9,10 +9,12 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyDGw6_H9IKWhlb7dhU5UHVxfly1LvOT6Qo",
   authDomain: "react-native-hw-d116c.firebaseapp.com",
+  databaseURL: "https://react-native-hw-d116c-default-rtdb.firebaseio.com",
   projectId: "react-native-hw-d116c",
   storageBucket: "react-native-hw-d116c.appspot.com",
   messagingSenderId: "515724937374",
-  appId: "1:515724937374:web:55ac4580de60c29bc9538f",
+  appId: "1:515724937374:web:f60590b717c4d366c9538f"
+
 };
 
 // Initialize Firebase
